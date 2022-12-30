e0dac1b1a9902e59160b176e213cd81b89b9ac9c
pessanitalo <italopessan@gmail.com>
2022-12-30 14:55:44 -0300

    Obtendo o id da rota

--- Front/src/app/emprestimo/novo-emprestimo/novo-emprestimo.component.ts
+++ Front/src/app/emprestimo/novo-emprestimo/novo-emprestimo.component.ts
@@ -1,6 +1,5 @@
 import { EmprestimoService } from 'src/app/emprestimo/services/emprestimo.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -13,18 +12,20 @@ export class NovoEmprestimoComponent implements OnInit {
   public valorEmprestimo!: number;
   public quantidadeParcela!: number;
 
-  id: number = 3;
+  public id!: number;
 
-  constructor(private route: ActivatedRoute,
-    private fb: FormBuilder,
-    private emprestimoService: EmprestimoService
+  constructor(
+    private emprestimoService: EmprestimoService,
+    private router: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.id = this.router.snapshot.params['id'];
   }
 
   novoEmprestimo() {
     this.emprestimoService.update(this.valorEmprestimo, this.quantidadeParcela, this.id)
-    .subscribe(sucesso => { alert('Emprestimo contratado com sucesso') },
-          falha => { console.log(falha) })}
+      .subscribe(sucesso => { alert('Emprestimo contratado com sucesso') },
+        falha => { console.log(falha) })
+  }
 }
