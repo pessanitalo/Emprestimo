46b8ff9f1f2adab42be60c335bb4e0a4259cc939
pessanitalo <italopessan@gmail.com>
2023-02-14 21:36:57 -0300

    Retirada do modulo front-end

--- Front/src/app/cliente/models/informationModel.ts
+++
@@ -1,7 +0,0 @@
-export interface informationModel{
-    id: number;
-    nome:string;
-    idade:number;
-    score:number;
-    saldoAtual:number;
-}
--- Front/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  mainUrlAPI: 'http://localhost:5110/api/'
-};
