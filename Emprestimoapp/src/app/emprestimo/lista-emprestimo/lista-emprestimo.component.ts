f04200def080406fd86884b3ed0cd094e2b0fcd6
italo pessan <italopessan@gmail.com>
2023-05-30 15:02:06 -0300

    limpeza de codigo

--- Emprestimoapp/src/app/emprestimo/lista-emprestimo/lista-emprestimo.component.ts
+++ Emprestimoapp/src/app/emprestimo/lista-emprestimo/lista-emprestimo.component.ts
@@ -1,7 +1,11 @@
+import { parcelas } from './../models/parcelas';
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Emprestimo } from '../models/emprestimo';
 import { EmprestimoService } from '../services/emprestimo.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { VisualizarParcelasComponent } from '../visualizar-parcelas/visualizar-parcelas.component';
 
 @Component({
   selector: 'app-lista-emprestimo',
@@ -11,19 +15,27 @@ import { EmprestimoService } from '../services/emprestimo.service';
 export class ListaEmprestimoComponent implements OnInit {
 
   public emprestimos!: Emprestimo[];
+  public parcelas!: parcelas[];
+
+  public parcela!: parcelas;
+
   errorMessage!: string;
   modalRef?: BsModalRef;
 
   public emprestimo!: Emprestimo;
 
+  public id = 3;
+
   constructor(
     private emprestimoService: EmprestimoService,
+    private router: ActivatedRoute,
+    private toastr: ToastrService,
+    private route: Router,
     private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.getList();
   }
-
 
   getList() {
     this.emprestimoService.list().subscribe(
@@ -32,10 +44,12 @@ export class ListaEmprestimoComponent implements OnInit {
     );
   }
 
-  openModal(template: TemplateRef<any>, emprestimo: Emprestimo) {
-    this.emprestimoService.obterPorId(emprestimo.id).subscribe((res) => {
-      this.emprestimo = res;
-      this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
-    })
+  processarSucesso(response: any) {
+    let toast = this.toastr.success('Emprestimo solicitado com sucesso!', 'Sucesso!');
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.route.navigate(['/cliente/list'])
+      });
+    }
   }
 }
