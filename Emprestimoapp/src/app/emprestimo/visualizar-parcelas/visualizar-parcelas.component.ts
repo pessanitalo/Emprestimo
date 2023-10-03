e4370ff1b4c47f34ee37e9e00a081b6c2bea36d0
italo pessan <italopessan@gmail.com>
2023-10-03 16:12:04 -0300

    paginação visualizar parcelas

--- Emprestimoapp/src/app/emprestimo/visualizar-parcelas/visualizar-parcelas.component.ts
+++ Emprestimoapp/src/app/emprestimo/visualizar-parcelas/visualizar-parcelas.component.ts
@@ -4,6 +4,7 @@ import { parcelas } from '../models/parcelas';
 import { BoletoService } from '../services/boleto.service';
 import { ToastrService } from 'ngx-toastr';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { PaginatedResult, Pagination } from 'src/app/cliente/models/pagination';
 
 @Component({
   selector: 'app-visualizar-parcelas',
@@ -16,6 +17,7 @@ export class VisualizarParcelasComponent implements OnInit {
   id!: number;
   public clienteId: number;
   parcelas!: parcelas[];
+  public pagination = {} as Pagination;
 
   parcela!: number;
   valor!: number;
@@ -27,17 +29,36 @@ export class VisualizarParcelasComponent implements OnInit {
     private router: Router,
     private modalService: BsModalService
 
-  ) {
-    this.parcelas = this.route.snapshot.data['parcelas'];
-  }
+  ) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.clienteId = this.route.snapshot.params['clienteId'];
+    this.pagination = {
+      currentPage: 1,
+      itemsPerPage: 12,
+      totalItems: 1,
+    } as Pagination;
+    this.carregarLista();
+  }
+
+  public pageChanged(event): void {
+    this.pagination.currentPage = event.page;
+    this.carregarLista();
+  }
+
+  public carregarLista(): void {
+    this.service.pagination(this.id, this.pagination.currentPage, this.pagination.itemsPerPage).subscribe(
+      (paginatedResult: PaginatedResult<parcelas[]>) => {
+        this.parcelas = paginatedResult.result;
+        this.pagination = paginatedResult.pagination;
+      },
+      falha => { console.log(falha) }
+    )
   }
 
   pagarParcela(numeroparcela: number) {
-    this.service.pagarParcela(this.clienteId,numeroparcela, this.id)
+    this.service.pagarParcela(this.clienteId, numeroparcela, this.id)
       .subscribe(sucesso => { this.processarSucesso(sucesso) },
         falha => { this.processarFalha(falha) }),
       this.closeModal();
