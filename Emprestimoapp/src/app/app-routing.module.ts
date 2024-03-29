fa0a081523907376875dae8c8877cd11d1c2e68a
italo pessan <italopessan@gmail.com>
2024-03-29 14:54:48 -0300

    rota menu-cadr

--- Emprestimoapp/src/app/app-routing.module.ts
+++ Emprestimoapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './navegacao/menu/menu.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+
   {
     path: 'cliente',
     loadChildren: () => import('./cliente/cliente.module')
